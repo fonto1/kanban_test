0c187c78dad0815f0831d800ca9d44926153d976
Fon To <fon.to@slu.se>
2019-10-10 08:54:59 +0200

    init

---
+++ script.js
@@ -0,0 +1,42 @@
+let data = {
+    icebox : ["hoppa","gråta"],
+    todo : ["leka"],
+    doing :["äta"],
+    test : ["jobba", "läsa"],
+    done : ["musik","måla"]
+}
+
+let ulIce = document.getElementById("icebox");
+let button = document.getElementById("button");
+let input = document.getElementById("input");
+
+button.addEventListener('click',create)
+
+function display() {
+document.getElementById("todo").innerHTML = data.todo.join('<br>');
+document.getElementById("test").innerHTML = data.test.join('<br>');
+document.getElementById("done").innerHTML = data.done.join('<br>');
+}
+
+//display();
+createDisplay()
+function createDisplay(){
+    ulIce.innerHTML ="";
+    
+    data.icebox.forEach(function(content){
+         
+         let li = document.createElement("li");
+         let text = document.createTextNode(content)
+         li.append(text);
+         li.setAttribute('draggable', true);
+         ulIce.append(li);
+
+         console.log(text);
+    })
+}
+
+
+function create(){
+   data.icebox.push(input.value);
+   createDisplay();
+}
