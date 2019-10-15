d8965b0c7a23442e8fd834a1cd44b97f73e192c3
Fon To <fon.to@slu.se>
2019-10-15 11:22:01 +0200

    updated index

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ let todo = document.getElementById("todo");
 let doing = document.getElementById("doing");
 let test = document.getElementById("test");
 let done = document.getElementById("done");
-let index = "index";
+let index;
+let data;
 
 
 
@@ -129,11 +130,11 @@ function push(evt){
 
 
 // ____________ localstorage
-let login_but = document.querySelector(".login_button")
-
- login_but.addEventListener('click',function(login){             
-   if(localStorage.getItem("index")) {
-       let temp = JSON.parse(localStorage.getItem("index"));
+    
+     
+    function ls(input){
+   if(localStorage.getItem(input)) {
+       let temp = JSON.parse(localStorage.getItem(input));
        data = temp;
        }
    // if not - create an empty []
@@ -147,17 +148,17 @@ let login_but = document.querySelector(".login_button")
        }
        };
    display();
-});
+};
 
 let logout_but = document.querySelector('.log_out');
 logout_but.addEventListener('click',function(logout){
-   localStorage.clear();
+ //  localStorage.clear();
    localStorage.setItem(index,JSON.stringify(data))
    logout1();
    console.log("logout");
 });
 
-
+let login_but = document.getElementById("login_button")
 login_but.addEventListener('click', ready)
 
 
@@ -180,7 +181,9 @@ function ready() {
                     let inputPass1 = inputPass.value;
                     user.push(inputUser1);
                     user.push(inputPass1);
-                     console.log(user);                   
+                     console.log(user);   
+                     index = inputUser1         
+                     ls(inputUser1)       
                 };
                 break; //bye bye for loop
             }
